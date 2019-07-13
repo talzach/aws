d6464584902763bb40a698e3d8c98d52da7609bc
tzach <tzach@panaya.com>
2019-07-13 16:54:15 +0300

    333

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,7 +11,7 @@ import Styles from '../styles.css';
 const Application = () => (
   <div className={Styles.container}>
     <header className={Styles.header}>
-      <h1 className={Styles.header__title}>Drissi</h1>
+      <h1 className={Styles.header__title}>Drissi33</h1>
     </header>
     <NoteListContainer/>
     <Switch>
